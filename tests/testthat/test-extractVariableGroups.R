df8da735d624f0da9fa7d06abe295df8eb019c69
Anastasis Giannousakis <11047746+giannou@users.noreply.github.com>
2021-02-19 15:05:23 +0100

    improve tests

--- tests/testthat/test-extractVariableGroups.R
+++ tests/testthat/test-extractVariableGroups.R
@@ -1,17 +1,47 @@
 context("extractVariableGroups Test")
 
 test_that("check that extractVariableGroups correctly extracts variable groups", {
-  x <- c("a|+|1|+|aa","a|+|2|abc","a|+|1|+|bb","a|+|1|+|cc","a|+|3|+|aa","a|+|3|+|bb","a","a|+|3")
-  res <- list("a"=c("a|+|1|+|aa","a|+|2|abc","a|+|1|+|bb","a|+|1|+|cc","a|+|3|+|aa","a|+|3|+|bb","a|+|3"),
-              "a|+|1"=c("a|+|1|+|aa","a|+|1|+|bb","a|+|1|+|cc"),
-              "a|+|3"=c("a|+|3|+|aa","a|+|3|+|bb"))
+  x1 <- c("a|+|1|+|aa",
+         "a|+|2|abc",
+         "a|+|1|+|bb",
+         "a|+|1|+|cc",
+         "a|+|3|+|aa",
+         "a|+|3|+|bb",
+         "a",
+         "a|+|3",
+         "a|++|bla",
+         "a|++|blu",
+         "a|+++|ble",
+         "a|+++|blet")
 
+  x2 <- c("a|+|1|+|aa",
+         "a|+|2|abc",
+         "a|+|1|+|bb",
+         "a|+|1|+|cc",
+         "a|+|3|+|aa",
+         "a|+|3|+|bb",
+         "a",
+         "a2",
+         "a|+|3",
+         "a2|+|bla",
+         "a2|+|blu",
+         "a3|++|ble",
+         "a3|++|blet")
   
-  expect_mapequal(extractVariableGroups(x),res)
-  expect_mapequal(extractVariableGroups(x,keepOrigNames = T),res)
+  res1 <- list("a"=c("a|+|1|+|aa","a|+|2|abc","a|+|1|+|bb","a|+|1|+|cc","a|+|3|+|aa","a|+|3|+|bb","a|+|3"),
+              "a|+|1"=c("a|+|1|+|aa","a|+|1|+|bb","a|+|1|+|cc"),
+              "a|+|3"=c("a|+|3|+|aa","a|+|3|+|bb"),
+              "a 2"=c("a|++|bla","a|++|blu"),
+              "a 3"=c("a|+++|ble" ,"a|+++|blet"))
+
+  res2 <- list("a"=c("a|+|1|+|aa","a|+|2|abc","a|+|1|+|bb","a|+|1|+|cc","a|+|3|+|aa","a|+|3|+|bb","a|+|3"),
+               "a|+|1"=c("a|+|1|+|aa","a|+|1|+|bb","a|+|1|+|cc"),
+               "a|+|3"=c("a|+|3|+|aa","a|+|3|+|bb"),
+               "a2"=c("a2|+|bla","a2|+|blu"),
+               "a3"=c("a3|++|ble" ,"a3|++|blet"))
   
-  x <- as.character(read.csv(system.file("extdata","vars.csv",package = "mip"))[,1])
-  expect_error(extractVariableGroups(x), NA)
-  expect_error(extractVariableGroups(x,keepOrigNames = T), NA)
+  expect_mapequal(extractVariableGroups(x1),res1)
+  expect_mapequal(extractVariableGroups(x2,keepOrigNames = T),res2)
   
+
 })

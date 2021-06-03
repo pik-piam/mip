7712c39a28335dd283b27f062a82151421e93f50
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2021-06-03 11:25:20 +0200

    helpful error message

--- R/getPlotData.R
+++ R/getPlotData.R
@@ -15,7 +15,7 @@ getPlotData <- function(filePaths, ...) {
   for (i in seq_along(filePaths)) {
     gdxContent <- readGDX(filePaths[[i]], ..., restore_zeros = FALSE)
 
-    # ensure we deal with a list even if ...length() == 1
+    # ensure gdxContent is a list even if ...length() == 1
     if (is.magpie(gdxContent)) {
       gdxContent <- list(gdxContent)
       names(gdxContent) <- ..1
@@ -23,10 +23,20 @@ getPlotData <- function(filePaths, ...) {
 
     # convert to dataframe, add iteration and variable value and append to plotData
     for (j in seq_along(gdxContent)) {
-      df <- magclass::as.data.frame(gdxContent[[j]], rev = 2)
-      df$variable <- names(gdxContent)[[j]]
-      df$iteration <- as.factor(i)
-      plotData <- rbind(plotData, df)
+      x <- magclass::as.data.frame(gdxContent[[j]], rev = 2)
+      x$variable <- names(gdxContent)[[j]]
+      x$iteration <- as.factor(i)
+      x <- x[, c(ncol(x), ncol(x) - 1, seq_len(ncol(x) - 2))] # move columns iteration & variable to front
+      plotData <- tryCatch(rbind(plotData, x),
+        error = function(error) {
+          message(paste0(
+            "Cannot merge with previous data, skipping variable ", names(gdxContent)[[j]],
+            "\nreason: the previous columns\n", paste(colnames(plotData), collapse = ", "), "\nare incompatible to\n",
+            paste(colnames(x), collapse = ", "), "\noriginal error message: ", error
+          ))
+          return(plotData)
+        }
+      )
     }
   }
   return(plotData)

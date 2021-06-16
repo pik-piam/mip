7656340fa5df9d8c8815b18197c6ca851c771b88
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2021-06-16 10:24:13 +0200

    rewrite getPlotData

--- R/getPlotData.R
+++ R/getPlotData.R
@@ -1,54 +1,43 @@
 #' getPlotData
 #'
-#' Get ready-to-plot data from a gdx file. Data for multiple symbols can be extracted and combined if they have the same
-#' domains.
+#' Get ready-to-plot data from one or more gdx files.
 #'
-#' @param pathToGdx Path to a single gdx file.
-#' @param symbolNames A vector of names of symbols to be extracted from the gdx. For each symbol the first two domains
-#' should represent iteration and year, they are converted to integer.
+#' @param pathToGdx Path to one or more gdx files. If multiple paths are provided each one represents data after a
+#' specific iteration. The order of paths should match iteration order, e.g. pathToGdx[1] should hold data for
+#' the first iteration, pathToGdx[2] for the second iteration etc.
+#' @param symbolName The name of a symbol to be extracted from gdx.
 #' @param compress Logical, passed to gdxrrw::rgdx.param. Ensures factor levels are equal to unique elements.
 #' @param ... Additional arguments passed to gdxrrw::rgdx.param.
-#' @return A data frame combined from data frames extracted by gdxrrw::rgdx.param, where an additional "symbol" column
-#' is holding the corresponding symbol name from symbolNames. The last column is always holding the actual values and is
-#' called "value".
+#' @return A data frame with data from the given gdx file(s). If multiple gdx files are provided an additional
+#' "iteration" column is added. The iteration value will be 1 for all data rows from the first gdx, 2 for the second etc.
 #' @author Pascal Führlich
 #' @seealso \code{\link{plotIterations}}
 #' @importFrom gdxrrw rgdx.param
 #' @export
-getPlotData <- function(pathToGdx, symbolNames, compress = TRUE, ...) {
-  factorToInt <- function(aFactor) {
-    return(as.integer(levels(aFactor)[aFactor]))
-  }
-
-  # read in data for a single symbol and combine it with previous data
-  reduceFunction <- function(combinedDataframe, symbolName) {
-    x <- gdxrrw::rgdx.param(pathToGdx, symbolName, compress = compress, ...)
-    x[, 1] <- factorToInt(x[, 1]) # expecting column 1 to represent iteration
-    x[, 2] <- factorToInt(x[, 2]) # expecting column 2 to represent year
-
-    # add symbol column and rename symbol value column to just value so data for multiple symbols can be combined
-    x$symbol <- as.factor(symbolName)
-    names(x)[names(x) == symbolName] <- "value"
-
-    # ensure value is the last column
-    valueColumn <- x$value
-    x <- x[, names(x) != "value"]
-    x$value <- valueColumn
-
-    # combine data into a single data frame
-    combinedDataframe <- tryCatch(
-      rbind(combinedDataframe, x),
-      error = function(error) {
-        warning(paste0(
-          "WARNING: Cannot merge with previous data, skipping symbol ", symbolName,
-          "\nreason: the previous columns\n", paste(colnames(combinedDataframe), collapse = ", "),
-          "\nare incompatible to\n", paste(colnames(x), collapse = ", "), "\noriginal error message: ", error
-        ))
-        return(combinedDataframe)
+getPlotData <- function(pathToGdx, symbolName, compress = TRUE, ...) {
+  stopifnot(length(symbolName) == 1)
+  if (length(pathToGdx) == 1) {
+    plotData <- rgdx.param(pathToGdx, symbolName, compress = compress, ...)
+  } else {
+    plotData <- NULL
+    for (i in seq_along(pathToGdx)) {
+      if (!grepl(paste0("[^0-9]0*", i, "[^0-9]"), pathToGdx[[i]])) {
+        warning(
+          'WARNING: "', pathToGdx[[i]], '" should contain data for iteration ', i,
+          ' but that path does not contain "', i,
+          '" - are file paths missing/ordered incorrectly? Consider using gtools::mixedsort\n'
+        )
       }
-    )
-    return(combinedDataframe)
+
+      gdxContent <- rgdx.param(pathToGdx[[i]], symbolName, compress = compress, ...)
+      gdxContent$iteration <- i
+      plotData <- rbind(plotData, gdxContent)
+    }
   }
 
-  return(Reduce(reduceFunction, symbolNames, NULL))
+  names(plotData)[names(plotData) == symbolName] <- "value"
+  plotData$iteration <- as.integer(plotData$iteration)
+  attr(plotData, "domains") <- NULL
+  attr(plotData, "domInfo") <- NULL
+  return(plotData)
 }
--- tests/testthat/test-getPlotData.R
+++ tests/testthat/test-getPlotData.R
@@ -1,4 +1,4 @@
-test_that("getPlotData works for single symbol", {
+test_that("getPlotData works for a single gdx file", {
   testData <- data.frame(
     iteration = as.factor(rep(1:4, each = 4)),
     year = as.factor(c(2000, 2000, 3000, 3000)),
@@ -6,7 +6,7 @@ test_that("getPlotData works for single symbol", {
     testSymbolName = rep(1:4, each = 4) + 0.1
   )
   attr(testData, "symName") <- "testSymbolName"
-  attr(testData, "domains") <- colnames(testData)
+  attr(testData, "domains") <- c("iteration", "year", "region")
 
   # write test data to gdx file
   testFile <- paste0(tempdir(), "/test.gdx")
@@ -16,78 +16,43 @@ test_that("getPlotData works for single symbol", {
     iteration = rep(1:4, each = 4),
     year = c(2000, 2000, 3000, 3000),
     region = as.factor(c("ABC", "XYZ")),
-    symbol = as.factor("testSymbolName"),
     value = rep(1:4, each = 4) + 0.1
   )
+  expected$year <- as.factor(expected$year)
+  attr(expected, "symName") <- "testSymbolName"
+
   expect_equal(getPlotData(testFile, "testSymbolName"), expected)
 })
 
-test_that("getPlotData works for multiple symbols", {
+test_that("getPlotData works for multiple gdx files", {
+  testDir <- tempdir()
+
   testData1 <- data.frame(
-    iteration = as.factor(rep(1:4, each = 4)),
-    year = as.factor(c(2000, 2000, 3000, 3000)),
+    year = as.factor(rep(2000 + 0:7, each = 2)),
     region = as.factor(c("ABC", "XYZ")),
-    testSymbolName1 = rep(1:4, each = 4) + 0.1
+    testSymbolName = rep(1:4, each = 4) + 0.1
   )
-  attr(testData1, "symName") <- "testSymbolName1"
-  attr(testData1, "domains") <- colnames(testData1)
+  attr(testData1, "symName") <- "testSymbolName"
+  attr(testData1, "domains") <- c("year", "region")
+  gdxrrw::wgdx.lst(paste0(testDir, "/test1.gdx"), testData1)
 
   testData2 <- data.frame(
-    iteration = as.factor(rep(1:4, each = 4)),
-    year = as.factor(c(2000, 2000, 3000, 3000)),
+    year = as.factor(rep(2000 + 0:7, each = 2)),
     region = as.factor(c("ABC", "XYZ")),
-    testSymbolName2 = rep(1:4, each = 4) + 0.2
+    testSymbolName = rep(1:4, each = 4) + 0.2
   )
-  attr(testData2, "symName") <- "testSymbolName2"
-  attr(testData2, "domains") <- colnames(testData2)
-
-  # write test data to gdx file
-  testFile <- paste0(tempdir(), "/test.gdx")
-  gdxrrw::wgdx.lst(testFile, testData1, testData2)
+  attr(testData2, "symName") <- "testSymbolName"
+  attr(testData2, "domains") <- c("year", "region")
+  gdxrrw::wgdx.lst(paste0(testDir, "/test2.gdx"), testData2)
 
   expected <- data.frame(
-    iteration = rep(1:4, each = 4),
-    year = c(2000, 2000, 3000, 3000),
+    year = as.factor(c(rep(2000 + 0:7, each = 2), rep(2000 + 0:7, each = 2))),
     region = as.factor(c("ABC", "XYZ")),
-    symbol = as.factor(c(rep("testSymbolName1", 16), rep("testSymbolName2", 16))),
-    value = c(rep(1:4, each = 4) + 0.1, rep(1:4, each = 4) + 0.2)
+    value = c(rep(1:4, each = 4) + 0.1, rep(1:4, each = 4) + 0.2),
+    iteration = rep(1:2, each = 16)
   )
-  expect_equal(getPlotData(testFile, c("testSymbolName1", "testSymbolName2")), expected)
-})
+  attr(expected, "symName") <- "testSymbolName"
 
-test_that("getPlotData works warns when combining incompatible data", {
-  testData1 <- data.frame(
-    iteration = as.factor(rep(1:4, each = 4)),
-    year = as.factor(c(2000, 2000, 3000, 3000)),
-    region = as.factor(c("ABC", "XYZ")),
-    testSymbolName1 = rep(1:4, each = 4) + 0.1
-  )
-  attr(testData1, "symName") <- "testSymbolName1"
-  attr(testData1, "domains") <- colnames(testData1)
-
-  testData2 <- data.frame(
-    iteration = as.factor(rep(1:4, each = 4)),
-    year = as.factor(c(2000, 2000, 3000, 3000)),
-    region2 = as.factor(c("ABC", "XYZ")),
-    testSymbolName2 = rep(1:4, each = 4) + 0.2
-  )
-  attr(testData2, "symName") <- "testSymbolName2"
-  attr(testData2, "domains") <- colnames(testData2)
-
-  # write test data to gdx file
-  testFile <- paste0(tempdir(), "/test.gdx")
-  gdxrrw::wgdx.lst(testFile, testData1, testData2)
-
-  expected <- data.frame(
-    iteration = rep(1:4, each = 4),
-    year = c(2000, 2000, 3000, 3000),
-    region = as.factor(c("ABC", "XYZ")),
-    symbol = as.factor("testSymbolName1"),
-    value = rep(1:4, each = 4) + 0.1
-  )
-  plotData <- expect_warning(
-    getPlotData(testFile, c("testSymbolName1", "testSymbolName2")),
-    "Cannot merge with previous data, skipping symbol testSymbolName2"
-  )
-  expect_equal(plotData, expected)
+  actual <- getPlotData(paste0(testDir, "/test", 1:2, ".gdx"), "testSymbolName")
+  expect_equal(actual, expected)
 })
